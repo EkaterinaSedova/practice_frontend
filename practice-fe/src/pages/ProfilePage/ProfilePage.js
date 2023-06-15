e8004d2a86936f918b3fe0176ab7037a818f8343
Екатерина Седова <katena.sedova@gmail.com>
2023-06-15 15:09:33 +0300

    получаю посты пользователя и отображаю их в его профиле (завтра сделаю посты красивыми)

--- practice-fe/src/pages/ProfilePage/ProfilePage.js
+++ practice-fe/src/pages/ProfilePage/ProfilePage.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../../components/Header/Header";
 import {useAuth} from "../../auth";
+import {fetchPostsByUser} from "../../service/postAPI";
+import Post from "../../components/Post/Post";
 
 const ProfilePage = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const {currentUser} = useAuth();
+
+    useEffect(() => {
+        (async () => {
+            const data = await fetchPostsByUser(currentUser.id);
+            setPosts(data);
+            setLoading(false)
+        })()
+    }, [])
+
     return (
         <>
             <Header/>
@@ -14,7 +27,16 @@ const ProfilePage = () => {
                 <p>Last Name: {currentUser.lastname}</p>
                 <p>Sex: {currentUser.sex}</p>
             </div>
+            {loading ?
+                <div>loading posts...</div>
+                :
+                <>
+                    {posts.map((post) => <Post key={post.id} post={post}/>)}
+                </>
+
+            }
         </>
+
     );
 };
 
--- practice-fe/src/service/postAPI.js
+++ practice-fe/src/service/postAPI.js
@@ -5,3 +5,9 @@ export const fetchPosts = async () => {
         const {data} = await $auth_host.get('/posts');
         return data;
 }
+
+export const fetchPostsByUser = async (id) => {
+        const {data} = await $auth_host.get('/posts/user/' + id);
+        console.log('hi from postAPI', data)
+        return data;
+}
